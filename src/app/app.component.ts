013295d40db1d5a16dbaa77d03f0d76fb06e78a7
Yohari Maza <yoharimaza@gmail.com>
2023-07-28 16:28:45 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'capi_examen_front_yohari';
+}
